c4681dea96d4f4805843793e41303dca9be2ef84
doingsllc <peterobijiofor@gmail.com>
2023-11-20 15:39:13 +0000

    updated

--- main.tf
+++ main.tf
@@ -6,10 +6,9 @@ data "digitalocean_ssh_key" "doings-dokey" {
 
 # Creating a Digital Ocean Droplet
 resource "digitalocean_droplet" "doings-droplet" {
-  image    = var.image
+  image    = var.droplet_image
   name     = var.droplet_name
   region   = var.droplet_region
-  size     = var.image_size
+  size     = var.droplet_size
   ssh_keys = [data.digitalocean_ssh_key.doings-dokey.id]
 }
-
